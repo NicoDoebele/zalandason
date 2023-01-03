336750b7a96c772519d8f0c7092961f779c7ac74
Nico Döbele <nico.doebele@googlemail.com>
2023-01-03 13:43:26 +0100

    improved article ui

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -7,8 +7,10 @@ export default async function Articles() {
 
     return (
         <>
-            <div className="w-screen flex flex-col gap-2 items-center mt-6">
-                {articles.map((article: any) => (<Article key={article.articleID} name={article.name} price={article.price} />))}
+            <div className="w-screen flex justify-center">
+                <div className="w-3/5 flex gap-2 mt-6 justify-center flex-wrap">
+                    {articles.map((article: any) => (<Article key={article.articleID} name={article.name} price={article.price} />))}
+                </div>
             </div>
         </>
     )
--- components/Article.tsx
+++ components/Article.tsx
@@ -1,13 +1,25 @@
+"use client"
+
 interface articleProps {
     name: string
     price: number
 }
 
+const addToCart = () => {
+
+}
+
 export default function Article({name, price}: articleProps) {
     return (<>
     
-        <div className="flex w-fit p-1 border-gray-300 border-2 rounded-md bg-gray-50">
-            {name} - {price}$
+        <div className="flex flex-col w-fit p-1 border-gray-300 border-2 rounded-md bg-gray-50 gap-2">
+            <div className="w-72 h-72 rounded-md bg-gray-300">
+
+            </div>
+            <div className="flex justify-center gap-2 items-center">
+                <span>{name} - {price}$</span>
+                <button onClick={addToCart} className="bg-green-200 rounded-md hover:bg-green-300 p-1 border-2 border-gray-300">Add to cart</button>
+            </div>
         </div>
     
     </>)
