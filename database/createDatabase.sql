6485df993acf4f73908529d8f7a9019b8ef7a1f7
Nico Döbele <nico.doebele@googlemail.com>
2023-03-24 15:12:25 +0100

    fixed createDatabase.sql

--- database/createDatabase.sql
+++ database/createDatabase.sql
@@ -7,12 +7,12 @@ create table articles(
     articleID int PRIMARY KEY,
     name varchar(100),
     price double
-)
+);
 
 insert into articles values(
     1, "Table", 18.99
-)
+);
 
 insert into articles values(
     2, "Chair", 11.99
-)
+);
